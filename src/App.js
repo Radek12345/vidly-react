cdcc17f8d7134047f0c2dc8236559550d760ecef
Radek12345 <gosciminskiradoslaw@gmail.com>
2019-05-06 18:09:41 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import LoginForm from "./components/loginForm";
 
 function App() {
   return (
     <main className="container">
-      <Movies />
+      <LoginForm />
     </main>
   );
 }
